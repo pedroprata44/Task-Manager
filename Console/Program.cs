ce2d13e02e3a2bcc7142fc987d85837bb83268cf
pedroprata44 <contato.pedroprata@gmail.com>
2024-10-29 12:55:25 -0300

    feat: remove entry point

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,9 +1,6 @@
 ﻿namespace Console{
     public class Program{
         static void Main(string[] args){
-            Manager manager = new();
-            manager.AddTask("task 1");
-            System.Console.WriteLine(manager.ListTasks());
         }
     }
 }
