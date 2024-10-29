16b5dc9222065c30c63a6e06f74f236235bf5369
Pedro Prata <contato.pedroprata@gmail.com>
2024-10-29 09:01:35 -0300

    feat: first commit

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -4,9 +4,7 @@ namespace Tests;
 
 public class UnitTest1: IDisposable
 {
-    Manager manager;
     public UnitTest1(){
-        manager = new ();
     }
     public void Dispose(){
         System.Console.WriteLine("Hello");
@@ -15,6 +13,7 @@ public class UnitTest1: IDisposable
     [Fact]
     public void Test1()
     {
+        Manager manager = new Manager();
         var inputTask = new {description = "task"};
         Console.Task task = manager.AddTask(inputTask.description);
         Assert.Equal(inputTask.description, task.Description);
