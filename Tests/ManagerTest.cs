b7cdf9939b0b71209c515eec475cbc335c0f6cee
pedroprata44 <contato.pedroprata@gmail.com>
2024-10-29 15:13:20 -0300

    test: add tests to use cases get remove update list

--- Tests/ManagerTest.cs
+++ Tests/ManagerTest.cs
@@ -8,8 +8,7 @@ public class ManagerTest{
         manager = new ();
     }
     [Fact]
-    public void ShouldAddTaskToTaskList()
-    {
+    public void ShouldAddTaskToTaskList(){
         // Given
         var inputTask = new {
             description = "task"
@@ -18,5 +17,59 @@ public class ManagerTest{
         Console.Task task = manager.AddTask(inputTask.description);
         // Then
         Assert.Equal(inputTask.description, task.Description);
+    }
+    [Fact]
+    public void ShouldGetTaskInTaskList(){
+        // Given
+        var inputTask = new {
+            description = "task"
+        };
+        // When
+        Console.Task task = manager.AddTask(inputTask.description);
+        Console.Task taskGetted = manager.GetTask(task.Id);
+        // Then
+        Assert.Equal(task, taskGetted);
+    }
+    [Fact]
+    public void ShouldRemoveTaskInTheList(){
+        // Given
+        var inputTask = new {
+            description = "task"
+        };
+        // When
+        var task = manager.AddTask(inputTask.description);
+        manager.RemoveTask(task.Id);
+        var exception = Assert.Throws<Exception>(() => manager.RemoveTask(task.Id));
+        // Then
+        Assert.Equal("Task not found!", exception.Message);
+    }
+    [Fact]
+    public void ShouldUpdateTaskInTheList(){
+        // Given
+        var inputTask = new {
+            description = "task"
+        };
+        var updateTask = new {
+            updateDescription = "task01"
+        };
+        // When
+        var task = manager.AddTask(inputTask.description);
+        manager.UpdateTask(task.Id, updateTask.updateDescription);
+        var newTask = manager.GetTask(task.Id);
+        // Then
+        Assert.Equal(updateTask.updateDescription, newTask.Description);
+    }
+    [Fact]
+    public void ShouldListTasksInTheList()
+    {
+        // Given
+        var inputTask = new {
+            description = "task"
+        };
+        // When
+        Console.Task task = manager.AddTask(inputTask.description);
+        var tasksListed = manager.ListTasks();
+        // Then
+        Assert.Equal($"{task.Id} {task.Description} {task.Completed}\n", tasksListed);
     }
 }
