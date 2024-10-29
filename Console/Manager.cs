5aeab36bfc9f805d49fbfecbecefa4393cfd3998
pedroprata44 <contato.pedroprata@gmail.com>
2024-10-29 12:55:12 -0300

    feat: rename tasks to tasklist

--- Console/Manager.cs
+++ Console/Manager.cs
@@ -1,27 +1,27 @@
 namespace Console{
     public class Manager{
-        public readonly List<Task> tasks = [];
+        public readonly List<Task> taskList = [];
         public Task AddTask(string description){
             Task task = new(description);
-            tasks.Add(task);
+            taskList.Add(task);
             return task;
         }
         public string RemoveTask(Guid id){
-            tasks.Remove(tasks.FirstOrDefault(t => t.Id == id) ?? throw new Exception("Task not found!"));
+            taskList.Remove(taskList.FirstOrDefault(t => t.Id == id) ?? throw new Exception("Task not found!"));
             return "";
         }
         public string UpdateTask(Guid Id, string description){
-            Task task = tasks.FirstOrDefault(t => t.Id == Id) ?? throw new Exception("Task not found");
+            Task task = taskList.FirstOrDefault(t => t.Id == Id) ?? throw new Exception("Task not found");
             task.Description = description;
             return "";
         }
         public string ListTasks(){
-            string taskList = "";
-            if(tasks.Count == 0) throw new Exception("List is Empty");
-            foreach(Task task in tasks){
-                taskList += $"{task.Id} {task.Description} {task.Completed}\n";
+            string output = "";
+            if(taskList.Count == 0) throw new Exception("List is Empty");
+            foreach(Task task in taskList){
+                output += $"{task.Id} {task.Description} {task.Completed}\n";
             }
-            return taskList;
+            return output;
         }
     }
 }
